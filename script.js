6582ffb2578e1951ff2c8156d070fc6d504fc7b9
Ana Clara <anasmatos28@gmail.com>
2022-02-10 02:46:25 -0300

    add menu

---
+++ script.js
@@ -0,0 +1,12 @@
+//User ID
+const nomeUsuario = prompt("Digite seu nome")
+
+//Menu
+function abrirMenu(){
+    document.getElementById("menu").style.display = "block";
+}
+
+function fecharMenu(){
+    document.getElementById("menu").style.display = "none";
+}
+
