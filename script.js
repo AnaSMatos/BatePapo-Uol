457f06f1bc2612994de22d2ed0c40a43c6ced43e
Ana Clara <anasmatos28@gmail.com>
2022-02-11 03:27:09 -0300

    login e print das msgs na tela

--- script.js
+++ script.js
@@ -1,5 +1,45 @@
 //User ID
-const nomeUsuario = prompt("Digite seu nome")
+const nomeUsuario = {name: prompt("Digite seu nome")}
+const requisicao = axios.post('https://mock-api.driven.com.br/api/v4/uol/participants', nomeUsuario)
+
+requisicao.then(loginSucesso);
+requisicao.catch(loginErro);
+
+//Entrou na sala
+function loginSucesso(){
+    //esconder login
+    const searchMessages = axios.get("https://mock-api.driven.com.br/api/v4/uol/messages")
+    searchMessages.then(printMessages);
+    //ver os participantes
+    //continuar online
+}
+
+function loginErro(){
+    alert("ops! algo deu errado! tente novamente")
+
+}
+
+function printMessages(response){
+    const allMessages = response.data;
+    let chat = document.querySelector(".chat");
+    for(let i=0; i < allMessages.length; i++){
+        if (allMessages[i].type = "status"){
+            chat.innerHTML += `
+                <div class="entrada-saida texto-msg">
+                    <time>(${allMessages[i].time})</time>
+                    <p class="acao"><span class="from">${allMessages[i].from}</span> ${allMessages[i].text}</p>
+                </div>
+            `
+        } else if (allMessages[i].type = "message"){
+
+
+        } else if (allMessages[i].type = "private_message"){
+
+        }
+    }
+}
+
+
 
 //Menu
 function abrirMenu(){
