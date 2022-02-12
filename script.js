76d539843f3f149b12ddf5ead1a8252516cb16c9
Ana Clara <anasmatos28@gmail.com>
2022-02-12 18:27:08 -0300

    chat atualizando + login atualizando

--- script.js
+++ script.js
@@ -1,42 +1,83 @@
 //User ID
 const nomeUsuario = {name: prompt("Digite seu nome")}
 const requisicao = axios.post('https://mock-api.driven.com.br/api/v4/uol/participants', nomeUsuario)
+let mensagemEnviada = {
+    from: nomeUsuario.name,
+    to:"Todos",
+    text:"",
+    type:"message"
+}
+
 
 requisicao.then(loginSucesso);
 requisicao.catch(loginErro);
 
+function logado(){
+    const usuarioOnline = axios.post('https://mock-api.driven.com.br/api/v4/uol/status', nomeUsuario);
+}
 //Entrou na sala
 function loginSucesso(){
     //esconder login
+    setInterval(getMessages, 3000);
+    //ver os participantes
+    setInterval(logado, 5000);
+}
+
+function getMessages(){
     const searchMessages = axios.get("https://mock-api.driven.com.br/api/v4/uol/messages")
     searchMessages.then(printMessages);
-    //ver os participantes
-    //continuar online
+    searchMessages.catch(failGetMessages);
 }
 
 function loginErro(){
-    alert("ops! algo deu errado! tente novamente")
+    alert("Esse nome de usuário já está sendo usado!")
+    const nomeUsuario = {name: prompt("Digite seu nome")}
+}
 
+function failGetMessages(){
+    alert("Erro ao carregar as mensagens");
+    window.location.reload();
 }
 
 function printMessages(response){
-    const allMessages = response.data;
+    let allMessages = response.data;
     let chat = document.querySelector(".chat");
     for(let i=0; i < allMessages.length; i++){
-        if (allMessages[i].type = "status"){
+        if (allMessages[i].type === "status"){
             chat.innerHTML += `
                 <div class="entrada-saida texto-msg">
                     <time>(${allMessages[i].time})</time>
                     <p class="acao"><span class="from">${allMessages[i].from}</span> ${allMessages[i].text}</p>
                 </div>
             `
-        } else if (allMessages[i].type = "message"){
+        } else if (allMessages[i].type === "message"){
+            chat.innerHTML += `
+                <div class="geral texto-msg">
+                    <time>(${allMessages[i].time})</time>
+                    <p class="acao"><span class="from">${allMessages[i].from}</span> para <span class="to">${allMessages[i].to}</span>:</p>
+                    <p class="mensagem">${allMessages[i].text}</p>
+                </div>
+            `
 
-
-        } else if (allMessages[i].type = "private_message"){
-
+        } else if (allMessages[i].type === "private_message"){
+            chat.innerHTML += `
+                <div class="privada texto-msg hidden">
+                    <time>(${allMessages[i].time})</time>
+                    <p class="acao"><span class="from">${allMessages[i].from}</span> reservadamente para <span class="to">${allMessages[i].to}</span>:</p>
+                    <p class="mensagem"> ${allMessages[i].text}</p>
+                </div>
+            
+            `
         }
     }
+}
+
+function sendMessage(){
+    let mensagem = document.querySelector("#send-message");
+    mensagemEnviada.text = mensagem.value;
+    postMessage = axios.post("https://mock-api.driven.com.br/api/v4/uol/messages", mensagemEnviada)
+
+    mensagem.value = "";
 }
 
 
